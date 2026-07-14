8b4a4789a7af61ad4d80ef711dea32eac89ee746
Thomas Skerbis <thomas.skerbis@mac.com>
2026-07-14 18:14:19 +0200

    beter cron

--- boot.php
+++ boot.php
@@ -33,6 +33,7 @@ if (rex::isBackend()) {
 
     if (rex_addon::get('cronjob')->isAvailable() && !rex::isSafeMode()) {
         rex_cronjob_manager::registerType('rex_statistics_hashremove_cronjob');
+        rex_cronjob_manager::registerType('rex_statistics_maintenance_cronjob');
     }
 
     $pagination_scroll = $addon->getConfig('statistics_scroll_pagination');
